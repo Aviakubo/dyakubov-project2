197b9210740fadc6299166742e471f4736435a78
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2021-10-04 16:43:26 -0400

    modified schema to have relational db behavior

--- models/user.js
+++ models/user.js
@@ -4,18 +4,21 @@ const Schema = mongoose.Schema;
 
 // User Schema
 const userSchema = Schema({
+    _id: Schema.Types.ObjectId,
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  people: [{ type: Schema.Types.ObjectId, ref: 'Stats'}],
 });
 
 // Stats Schema
 const statsSchema = Schema({
+    user_id: { type: Schema.Types.ObjectId, ref: 'User'},
     name: { type: String, required: true},
     height: { type: Number, required: true},
     weight: { type: Number, required: true},
     age: { type: Number, required: true},
     gender: { type: String, required: true},
-})
+});
 
 // User & Stats Model
 const User = mongoose.model('User', userSchema);
